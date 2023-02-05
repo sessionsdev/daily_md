f755e6c1c3af26667b1847d74cb996e31ccfe095
Jon Sessions <jon@sessionsdev.com>
2023-02-05 08:59:51 -0500

    various improvements and added test data

--- py-daily.py
+++ py-daily.py
@@ -3,54 +3,127 @@
 import argparse
 import configparser
 from datetime import datetime
+import re
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 file_path = config['DEFAULT']['file_path']
 date_format = config['DEFAULT']['date_format']
+date_format = "%Y-%m-%d %A"
+today = datetime.now().strftime(date_format)
 
-def create_header(content):
-    today = datetime.now().strftime(date_format)
+def create_today_header(lines=None):
+    lines = lines or []
     header = f'# {today}\n'
-    if content:
-        content.append('\n' + header)
-    else:
-        content.append(header)
-    return header
+    if not any(line.startswith(header) for line in lines):
+        lines.append(header + "\n")
+    return lines
 
-def find_header(content):
-    header = f'# {datetime.now().strftime("%Y-%m-%d")}'
-    for line in content:
+def get_or_create_today_header_index(lines=None):
+    lines = lines or []
+    header = f'# {today}\n'
+    for index, line in enumerate(lines):
         if line.startswith(header):
-            return line
-    return create_header(content)
+            return index
+    lines = create_today_header(lines)
+    return len(lines) - 1
 
-def append_todo(content, text):
-    header = find_header(content)
+
+def get_section_indices(lines, header_index):
+    next_header_index = 0
+    
+    for i, line in enumerate(lines[header_index+1:], start=header_index+1):
+        if line.startswith("#"):
+            next_header_index = i
+            break
+    
+    return (header_index, next_header_index-1 if next_header_index else len(lines)-1) 
+
+def append_todo(lines, text):
+    indexs = get_section_indices(lines, get_or_create_today_header_index(lines))
     todo_item = f'- [ ] {text}\n'
-    index = content.index(header)
-    content.insert(index + 1, todo_item)
+    if indexs[0] == indexs[1]:
+        lines.insert(indexs[1]+1, todo_item)
+    else:
+        lines.insert(indexs[1], todo_item)
 
-def append_log(content, text):
-    header = find_header(content)
+def print_section(lines, start, end):
+    return ''.join(lines[start:end])
+
+
+def find_matching_date_indices(date_pattern, lines):
+    pattern = re.compile(f'# {date_pattern.replace("*", "[0-9]+")}')
+    return tuple(i for i, line in enumerate(lines) if pattern.match(line))
+
+
+def print_date_sections(lines, date_pattern):
+    indices = find_matching_date_indices(date_pattern, lines)
+    section_indices = []
+    for index in indices:
+        section_indices.append(get_section_indices(lines, index))
+    
+    print("-----------------------------------------")
+    for section in section_indices:
+        print(print_section(lines, section[0], section[1]))
+        print("-----------------------------------------")
+
+def append_log(lines, text):
+    header = get_or_create_today_header_index(lines)
     log_item = f'- {text}\n'
-    index = content.index(header)
-    content.insert(index + 1, log_item)
+    index = lines.index(header)
+    lines.insert(index + 1, log_item)
 
-parser = argparse.ArgumentParser(description='Add a todo item to a file')
+
+def log_entry(args):
+    print("Logging entry:", args.log)
+
+def add_todo(args):
+    print("Adding to-do item:", args.todo)
+
+def migrate(args):
+    print("Migrating past uncompleted to-do items to today's section")
+
+def mark_complete(args):
+    print("Marking item as complete:", args.complete)
+
+def print_argument(args):
+    print("Printing argument:", args.print)
+
+
+parser = argparse.ArgumentParser(description="display_help")
+
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('-t', '--todo', help='Add a todo item with a checkbox')
-group.add_argument('-l', '--log', help='Add a log item with a dash')
+group.add_argument("-l", "--log", help="Log an entry")
+group.add_argument("-t", "--todo", help="Add a to-do item")
+group.add_argument("-m", "--migrate", action="store_true", help="Migrate past uncompleted todo items to today's section")
+group.add_argument("-c", "--complete", help="Mark an item as complete")
+group.add_argument("-p", "--print", help="Print an argument")
 
 
 args = parser.parse_args()
 
-with open(file_path, 'r+') as f:
-    content = f.readlines()
-    if args.todo:
-        append_todo(content, args.todo)
-    elif args.log:
-        append_log(content, args.log)
-    f.seek(0)
-    f.write(''.join(content))
+commands = {
+    'log': log_entry,
+    'todo': add_todo,
+    'migrate': migrate,
+    'complete': mark_complete,
+    'print': print_argument,
+}
+
+for key in commands.keys():
+    if getattr(args, key):
+        commands[key](args)
+        break
+
+# with open(file_path, 'r+') as f:
+#     lines = f.readlines()
+#     if args.todo:
+#         append_todo(lines, args.todo)
+#     elif args.log:
+#         append_log(lines, args.log)
+#     elif args.day:
+#         print_date_sections(lines, today)
+#     f.seek(0)
+#     f.write(''.join(lines))
+
--- todo.md
+++ todo.md
@@ -1,16 +1,62 @@
-# 2023-02-04 Saturday
-- test test
-- test test
-- test test
-- test test
-- test test
-- test test
-- test test
-- [ ] test test
-- [ ] test test
-- [ ] test test
-- [ ] test test
-- [ ] test test
-- [ ] test test
-- [ ] test test
-- [ ] test test
+# 2023-01-30 Monday
+
+- trying out a single file daily note again. I should maybe set up some scripting that can make changes to this note. Or parse it some way. This note will contain my tasks and logs for my day to day.
+- Discussed Mass Effect 2 with franz. Really excellent game. Maybe my favorite.
+- Script notes for potential daily note cli script
+- [Sed Command in Linux/Unix with examples - GeeksforGeeks](https://www.geeksforgeeks.org/sed-command-in-linux-unix-with-examples/)
+- learned about `.gitkeep` files that will preserve empty folders
+- meeting to introduce new employee to team.
+- lunch - egg salad
+
+# 2023-01-31 Tuesday
+
+- 11:30 Company all-hands today
+- 12:55 I love my wife
+- 16:24 spent a lot of time working on the bash script to modify this file. I like the idea of a cli tool that can allow me to rapidly log, like this
+- 16:25 I should also investigate making a javascript based plugin for obsidian. Thought the bash script is a bit more future proof
+- 1-on-1 with Andrew ran long. ~90 minutes
+- [>] send CN invoices to Kim
+- treat work sessions like going to a movie (The One Thing - Gary Keller)
+- batch tasks before you start
+- batching by project
+- batching by task
+- [>] buy more cheese powder
+
+# 2023-02-01 Wednesday
+
+- [>] send CN invoices to Kim
+- [>] buy more cheese powder
+- in therapy today, discussed self-improvement and balancing that against self-acceptence.
+- sprint-kickoff today. These seem like a waste of time to me, but maybe there's some benifit to the bi-weekly recaps
+- Coding nomads tax form -[[Form 1099-NEC (Rev. January 2022) - ACFrOgD72kVHFfEL6Rrju0VXdCBGmy9WTd5mva1DjVBfFK4jrwGZBCn_1cb2N8wFri1fUkcAD6JO9RG8woKimXPJUKHyBnAoTZ4AQY21adPcJjds7Gnj1BICnuTmb6ue3y4ErmlnzmQSntIPhq3k.pdf]]
+- [>] send CN tax form to kristen.
+
+# 2023-02-02 Thursday
+
+- Slept too long today. I need to get my sleep schedule in order
+- Going to try to be heads down today
+- [>] send CN invoices to Kim
+- [x] buy more cheese powder
+- [>] send CN tax form to kristen.
+- [[Setting up a playwright test suite]]
+- [Parks and Recreation | Verizon Chipotle Exxon (Digital Exclusive) - YouTube](https://www.youtube.com/watch?v=XFKoGtgg6Mo)
+- [What Do Most Mass Shooters Have in Common? They Bought Their Guns Legally. - The New York Times](https://www.nytimes.com/2022/05/16/us/politics/legal-gun-purchase-mass-shooting.html)
+
+# 2023-02-03 Friday
+
+- it's 00:30 in the morning. Feeling like I wasted the day, but I actualy worked much more than usual.
+- [ ] send CN invoices to Kim
+- [ ] send CN tax form to kristen.
+
+- BASH cleanUp and trap [Useful Patterns for Shell Scripts | Effective Shell](https://effective-shell.com/part-4-shell-scripting/useful-patterns-for-shell-scripts/)
+
+- [x] look into this tomorrow!!!
+
+
+
+# 2023-02-05 Sunday
+
+- [ ] test test test
+- [ ] test test test
+- [ ] test
+- [ ] test test test
